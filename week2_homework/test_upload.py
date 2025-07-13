42288fe5265a4f29bc18592d11ba7fb709f1b8d8
wu-liulin <wuliulin_560@qq.com>
2025-07-14 01:52:54 +0800

    second optimization

--- week2_homework/test_upload.py
+++
@@ -1,8 +0,0 @@
-# 测试上传文件函数的功能
-from core_api import upload_knowledge_file
-
-session_id = "abc123"
-file_path = r"D:\大二下资料\设计思维\6,思维发散.pdf"  # 替换为实际文件路径
-
-msg = upload_knowledge_file(session_id, file_path)
-print(msg)
